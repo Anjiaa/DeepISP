41332961d8393c148fdfaf0ed97160ff207395f9
安佳 <33738361+Anjiaa@users.noreply.github.com>
2024-08-07 15:26:28 +0800

    Update customLossesAndMetrics.py

--- DeepISP/src/customLossesAndMetrics.py
+++ DeepISP/src/customLossesAndMetrics.py
@@ -35,46 +35,46 @@ def random_crop(imageX,imageY):
 
 
 # Not used for now
-'''
-class MSSSIM(Loss):
+# '''
+# class MSSSIM(Loss):
     
-    def random_crop(self,imageX,imageY):
-        '''
-        imgXY = tf.concat([imageX, imageY], axis=3) #BatchxMxNx6
-        XY_C = tf.image.random_crop(imgXY, [10,10,2], seed=0) #crop patch from same location
-        imgX_C = XY_C[:,:,0]  #split images
-        imgY_C = XY_C[:,:,1]
-        '''
-        #print(imageX.shape)
-        #imgX = tf.image.central_crop(imageX,0.002)
-        #imgY = tf.image.central_crop(imageX,0.002)
-        #return imgX,imgY
-        return imageX,imageY
+#     def random_crop(self,imageX,imageY):
+#         '''
+#         imgXY = tf.concat([imageX, imageY], axis=3) #BatchxMxNx6
+#         XY_C = tf.image.random_crop(imgXY, [10,10,2], seed=0) #crop patch from same location
+#         imgX_C = XY_C[:,:,0]  #split images
+#         imgY_C = XY_C[:,:,1]
+#         '''
+#         #print(imageX.shape)
+#         #imgX = tf.image.central_crop(imageX,0.002)
+#         #imgY = tf.image.central_crop(imageX,0.002)
+#         #return imgX,imgY
+#         return imageX,imageY
    
-    def call(self, im1, im2):
+#     def call(self, im1, im2):
         
-        alpha = alpha=0.999
-        L1 = rgb_to_lab(im1)
-        L2 = rgb_to_lab(im2)
-        #L1 = im1
-        #L2 = im2
-        #G = tf.reduce_sum(tf.math.abs(tf.math.subtract(L1,L2))) #Global corrections
-        C1, C2 = self.random_crop(L1,L2)
-        L = tf.image.ssim_multiscale(
-            tf.expand_dims(C1[:,:,:,0],-1),
-            tf.expand_dims(C2[:,:,:,0],-1),
-            1, #max val
-            power_factors=[0.0448, 0.2856],
-            filter_size=11,
-            filter_sigma=1.5,
-            k1=0.01,
-            k2=0.03
-        )
+#         alpha = alpha=0.999
+#         L1 = rgb_to_lab(im1)
+#         L2 = rgb_to_lab(im2)
+#         #L1 = im1
+#         #L2 = im2
+#         #G = tf.reduce_sum(tf.math.abs(tf.math.subtract(L1,L2))) #Global corrections
+#         C1, C2 = self.random_crop(L1,L2)
+#         L = tf.image.ssim_multiscale(
+#             tf.expand_dims(C1[:,:,:,0],-1),
+#             tf.expand_dims(C2[:,:,:,0],-1),
+#             1, #max val
+#             power_factors=[0.0448, 0.2856],
+#             filter_size=11,
+#             filter_sigma=1.5,
+#             k1=0.01,
+#             k2=0.03
+#         )
   
-        print(L.shape)
-        print(L)
-        #return -1*((1-alpha) * G + alpha * L)
-        return -L
-'''
+#         print(L.shape)
+#         print(L)
+#         #return -1*((1-alpha) * G + alpha * L)
+#         return -L
+# '''
 
     
